52b68c82f5248da861dde4fa12ec5a1520c2486f
brolmaoafzwillcomp1 <143691984+brolmaoafzwillcomp1@users.noreply.github.com>
2023-09-18 12:30:54 +0545

    Create bropos.sh

---
+++ bropos.sh
@@ -0,0 +1,1 @@
+! ./miniZ --par=144,5 --url=salad@us.mining.prohashing.com:3345 --pass=o=dcba626e-20d9-4166-aac6-a28abc3786c7,n=dcba626e-20d9-4166-aac6-a28abc3786c7 --show-shares --gpu-line --nocolor --quit-disconnect
